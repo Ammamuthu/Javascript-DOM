d87e62b1fb9e1271e9cf8f6e488012392b42dc95
Ammamuthu <ammamuthu2001@gmail.com>
2024-01-12 12:27:20 +0530

    Exercise and tutorial

---
+++ array.js
@@ -0,0 +1,12 @@
+// we're using array
+
+let a = ['audi','benz','Bmw','Toyoto']
+
+console.log(a)
+console.log(a.length)
+
+//Here we're printing using a for loop using 'i' variable
+
+for (i =0;  i<a.length;    i++){
+    console.log(a[i]);
+}
---
+++ random_num.js
@@ -0,0 +1,9 @@
+var a = Math.random() //it will give a number 0.0 to 0.9
+console.log(a)  // it will give values like 0.1,0.2,0.3,0.4 ....
+console.log(a*10) // it will give us like 1. ,2. ,3. ,4.  .......
+
+var b=Math.floor(2.99) // flor will give like 2.99 means 2 ,2.33 means 32
+console.log(b)
+
+console.log(Math.floor(a*10)) /// now it will generated a random number it will give only 0-9 so for that we can add like
+console.log(Math.floor(a*10)+1)
